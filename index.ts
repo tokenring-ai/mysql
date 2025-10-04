12ca3c5ae12e7a72d54dbdb1139b6c435bfdf99b
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-04 12:50:36 -0700

    Implemented auto-configuration, that allows the package to automatically install into TokenRing apps

--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
-import {TokenRingPackage} from "@tokenring-ai/agent";
+import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
+import {DatabaseConfigSchema} from "@tokenring-ai/database";
+import DatabaseService from "@tokenring-ai/database/DatabaseService";
+import MySQLProvider from "./MySQLProvider.js";
 import packageJSON from './package.json' with {type: 'json'};
-
-export {default as MySQLProvider} from "./MySQLProvider.js";
 
 export const packageInfo: TokenRingPackage = {
   name: packageJSON.name,
   version: packageJSON.version,
-  description: packageJSON.description
+  description: packageJSON.description,
+  install(agentTeam: AgentTeam) {
+    const databaseConfig = agentTeam.getConfigSlice('database', DatabaseConfigSchema);
+    if (databaseConfig) {
+      agentTeam.services.waitForItemByType(DatabaseService).then(databaseService => {
+        for (const name in databaseConfig.providers) {
+          const provider = databaseConfig.providers[name];
+          if (provider.type === "mysql") {
+            databaseService.registerDatabase(name, new MySQLProvider(provider));
+          }
+        }
+      });
+    }
+  }
 };
+
+export {default as MySQLProvider} from "./MySQLProvider.js";
