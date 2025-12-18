307b37b1943361a09eda7f28dec29cd297d464f1
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:40 -0800

    Extract and modularize plugin logic into `plugin.ts`

---
+++ plugin.ts
@@ -0,0 +1,26 @@
+import TokenRingApp from "@tokenring-ai/app";
+import {DatabaseConfigSchema} from "@tokenring-ai/database";
+import DatabaseService from "@tokenring-ai/database/DatabaseService";
+import {TokenRingPlugin} from "@tokenring-ai/app";
+import MySQLProvider from "./MySQLProvider.js";
+import packageJSON from './package.json' with {type: 'json'};
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+    const databaseConfig = app.getConfigSlice('database', DatabaseConfigSchema);
+    if (databaseConfig) {
+      app.waitForService(DatabaseService, databaseService => {
+        for (const name in databaseConfig.providers) {
+          const provider = databaseConfig.providers[name];
+          if (provider.type === "mysql") {
+            databaseService.registerDatabase(name, new MySQLProvider(provider));
+          }
+        }
+      });
+    }
+  }
+} as TokenRingPlugin;
